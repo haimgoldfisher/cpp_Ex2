cd60f9e2268b7a058f28380b92e24dedfe61d304
Haim <haimngoldfisher@gmail.com>
2023-04-02 07:32:38 -0700

    start with part B

--- sources/card.cpp
+++ sources/card.cpp
@@ -2,5 +2,29 @@
 
 namespace ariel
 {
-    
+    Card::Card(int number, Suit symbol)
+    {
+        if(number > KING || number < 1)
+        {
+            throw("ILLEGAL NUMBER");
+            exception;
+        }
+        if(symbol < CLUBS || symbol > SPADES)
+        {
+            throw("ILLEGAL SUIT");
+            exception;
+        }
+        this->value = number;
+        this->suit = symbol;
+    }
+
+    int Card::getValue()
+    {
+        return this->value;
+    }
+
+    Suit Card::getSuit()
+    {
+        return this->suit;
+    }
 }
